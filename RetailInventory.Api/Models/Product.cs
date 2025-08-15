395d1c6eae2ad28350d31fb746e2e912b524175b
Lance <miguel2002622@gmail.com>
2025-08-15 23:16:00 +0800

    feat: add Product entity model for inventory

---
+++ RetailInventory.Api/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetailInventory.Api.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Name { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
