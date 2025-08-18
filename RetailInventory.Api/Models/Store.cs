ae3ef8ea8853dbceaf13ce4e78dc41d885baa708
Lance <miguel2002622@gmail.com>
2025-08-18 21:59:29 +0800

    feat(models): Add Store and User entities
    - Create Store model with Id, Name, Address
    - Create User model with Id, Username, PasswordHash, Role, StoreId
    - Add navigation properties for relationships

---
+++ RetailInventory.Api/Models/Store.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailInventory.Api.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(256)]
+        public string? Address { get; set; }
+
+        // Navigation property: One store can have many users
+        public ICollection<User>? Users { get; set; } = new List<User>();
+    }
+}
---
+++ RetailInventory.Api/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetailInventory.Api.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required, MaxLength(32)]
+        public string Role { get; set; } = "Staff"; // Roles can be "Staff", "Owner", "SystemAdmin"
+
+        // Foreign key to Store (Nullable for SystemAdmin)
+        public int? StoreId { get; set; }
+        [ForeignKey("StoreId")]
+        public Store? Store { get; set; }
+    }
+}
